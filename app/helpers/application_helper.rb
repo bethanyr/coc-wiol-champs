55e12ba3bf202151178c92fbcff986a70a668cca
Valerie Meyer <vmeyer9086@aol.com>
2019-03-29 04:47:23 -0400

    Added IC Classes to Application Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-  CLASS_LIST =  ['ISVM', 'ISVF', 'ISJVM', 'ISJVF', 'ISIM', 'ISIF', 'ISPM', 'ISPF']
+  CLASS_LIST =  ['ISVM', 'ISVF', 'ISJVM', 'ISJVF', 'ISIM', 'ISIF', 'ISPM', 'ISPF', 'ICVM', 'ICVF']
 
   def float_time_to_hhmmss(float_time)
     return if float_time.class == String
