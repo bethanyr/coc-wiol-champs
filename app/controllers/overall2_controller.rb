ede581279c51f19b7ca0e863db3dd2526ef2b859
Valerie Meyer <vmeyer9086@aol.com>
2019-04-03 15:45:26 -0400

    Files for adding Overall2 for 2nd results screen

---
+++ app/controllers/overall2_controller.rb
@@ -0,0 +1,20 @@
+class Overall2Controller < ApplicationController
+  def index
+    @isvm = Runner.find_by_sql("SELECT firstname, surname, school, day1_score, day2_score, (day1_score + day2_score) as total  FROM runners where entryclass = 'ISVM' and day1_score > 0 and day2_score > 0 order by total;")
+        @isvf = Runner.find_by_sql("SELECT firstname, surname, school, day1_score, day2_score, (day1_score + day2_score) as total  FROM runners where entryclass = 'ISVF' and day1_score > 0 and day2_score > 0 order by total;")
+        @isjvm = Runner.find_by_sql("SELECT firstname, surname, school, day1_score, day2_score, (day1_score + day2_score) as total  FROM runners where entryclass = 'ISJVM' and day1_score > 0 and day2_score > 0 order by total;")
+        @isjvf = Runner.find_by_sql("SELECT firstname, surname, school, day1_score, day2_score, (day1_score + day2_score) as total  FROM runners where entryclass = 'ISJVF' and day1_score > 0 and day2_score > 0 order by total;")
+        @isim = Runner.find_by_sql("SELECT firstname, surname, school, day1_score, day2_score, (day1_score + day2_score) as total  FROM runners where entryclass = 'ISIM' and day1_score > 0 and day2_score > 0 order by total;")
+        @isif = Runner.find_by_sql("SELECT firstname, surname, school, day1_score, day2_score, (day1_score + day2_score) as total  FROM runners where entryclass = 'ISIF' and day1_score > 0 and day2_score > 0 order by total;")
+        @ispm = Runner.find_by_sql("SELECT firstname, surname, school, day1_score, day2_score, (day1_score + day2_score) as total  FROM runners where entryclass = 'ISPM' and day1_score > 0 and day2_score > 0 order by total;")
+        @ispf = Runner.find_by_sql("SELECT firstname, surname, school, day1_score, day2_score, (day1_score + day2_score) as total  FROM runners where entryclass = 'ISPF' and day1_score > 0 and day2_score > 0 order by total;")
+        @icvm = Runner.find_by_sql("SELECT firstname, surname, school, day1_score, day2_score, (day1_score + day2_score) as total  FROM runners where entryclass = 'ICVM' and day1_score > 0 and day2_score > 0 order by total;")
+        @icvf = Runner.find_by_sql("SELECT firstname, surname, school, day1_score, day2_score, (day1_score + day2_score) as total  FROM runners where entryclass = 'ICVF' and day1_score > 0 and day2_score > 0 order by total;")
+        @icjvm = Runner.find_by_sql("SELECT firstname, surname, school, day1_score, day2_score, (day1_score + day2_score) as total  FROM runners where entryclass = 'ICJVM' and day1_score > 0 and day2_score > 0 order by total;")
+        @icjvf = Runner.find_by_sql("SELECT firstname, surname, school, day1_score, day2_score, (day1_score + day2_score) as total  FROM runners where entryclass = 'ICJVF' and day1_score > 0 and day2_score > 0 order by total;")
+        @isvf_jrotc = Runner.find_by_sql("SELECT firstname, surname, school, day1_score, day2_score, (day1_score + day2_score) as total  FROM runners where entryclass = 'ISVF' and jrotc = 'JROTC' and day1_score > 0 and day2_score > 0 order by total;")
+        @isvm_jrotc = Runner.find_by_sql("SELECT firstname, surname, school, day1_score, day2_score, (day1_score + day2_score) as total  FROM runners where entryclass = 'ISVM' and jrotc = 'JROTC' and day1_score > 0 and day2_score > 0 order by total;")
+        @isjvf_jrotc = Runner.find_by_sql("SELECT firstname, surname, school, day1_score, day2_score, (day1_score + day2_score) as total  FROM runners where entryclass = 'ISJVF' and jrotc = 'JROTC' and day1_score > 0 and day2_score > 0 order by total;")
+    @isjvm_jrotc = Runner.find_by_sql("SELECT firstname, surname, school, day1_score, day2_score, (day1_score + day2_score) as total  FROM runners where entryclass = 'ISJVM' and jrotc = 'JROTC' and day1_score > 0 and day2_score > 0 order by total;")
+end
+end
